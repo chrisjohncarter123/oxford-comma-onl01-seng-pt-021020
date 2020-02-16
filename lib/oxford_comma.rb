4560b844213c3b0cc101d65fbfe1733402fcfce7
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-16 05:46:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,11 @@
 def oxford_comma(array)
   
+  if(array.length == 2)
+    return "#{array[0]} and #{array[1]}"
   result = ""
   array.each_with_index do |word, index|
     remaining = array.length - index
+    
     if(index == 0 && remaining == 1)
       result = word
     elsif(index > 0 && remaining > 1)
