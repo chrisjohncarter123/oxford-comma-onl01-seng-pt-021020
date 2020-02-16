2b07f9906796e95655cfc616a4630fc59dfa77e7
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-16 05:38:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,13 @@
 def oxford_comma(array)
   
   result = ""
-  array.each_with_index {|word|
-    
+  array.each_with_index {|word, index|
+    remaining = array.length - index
+    if(index == 1)
+      result += " and #{word}"
+    else
+      result += ", #{word}"
     
   }
-
+  result
 end
